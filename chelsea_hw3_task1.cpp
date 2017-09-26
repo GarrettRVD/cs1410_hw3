66a143c935832b5e2fe7b7771f34b5fe9da26780
Garrett <garrettvandyke@mail.weber.edu>
2017-09-26 12:54:01 -0600

    Initial commit

---
+++ chelsea_hw3_task1.cpp
@@ -0,0 +1,21 @@
+// File: chelsea_hw3_task1.cpp
+// Created by Garrett Van Dyke on 9/26/2017 for CS1410.
+// Copyright (c) 2017 WSU
+
+#include <iostream>
+using namespace std;
+// Constants, Classes, Structures
+
+
+// Prototypes
+
+
+// Main Program Program
+int main(void)
+{
+
+
+    return 0;
+}
+
+// Function Definitions
